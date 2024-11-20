cb8f0173337eb66f547619ea8f1c7f7d05738233
MauroQ80 <mauroquesada80@gmail.com>
2024-11-19 20:23:01 -0600

    feat: banner added and changes in post 1

--- src/config.ts
+++ src/config.ts
@@ -15,30 +15,25 @@ export const siteConfig: SiteConfig = {
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
-    src: 'assets/images/Foto de Perfil.jpeg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    enable: true,
+    src: 'assets/images/Banner_.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
+      enable: true,         // Display the credit text of the banner image
+      text: 'Check Out My Latest Work',              // Credit text to be displayed
+      url: 'https://www.mclovin.site/'                // (Optional) URL link to the original artwork or artist's page
     }
   },
   toc: {
     enable: true,           // Display the table of contents on the right side of the post
     depth: 2                // Maximum heading depth to show in the table, from 1 to 3
   },
-  favicon: [  
+
+  favicon: [
     {
-      src: '/favicon/Hasbulla.png',    // Ruta de tu favicon en el directorio /public
-      sizes: '32x32',              // (Opcional) Define las dimensiones del favicon
+      src: '/favicon/Hasbulla.png', // Favicon para el tema claro
+      sizes: '32x32',               // Tamaño del favicon
     },
-     // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
   ]
 }
 
--- src/content/posts/Post 1/Book Preview.md
+++ src/content/posts/Post 1/Book Preview.md
@@ -8,7 +8,10 @@ draft: false
 ---
 
 # My Book
+## Book cover idea
+<img src="/favicon/Book_Cover.jpeg" width="310" height="210">
 
+## Why i want to write it?
 In a world that feels increasingly fast-paced and disconnected, my book, Reflejos del pasado: Valores que cambian el presente, seeks to explore the core values that once unified humanity. Many of these values—such as compassion, honesty, and a sense of community—seem to have faded with time. However, through thoughtful stories and historical reflections, I aim to show that these values are not lost. They live on, upheld by individuals and groups who remind us of the potential for good that still exists today.
 
 This book is an invitation to rediscover what connects us as humans and to see examples in modern society that prove these values remain relevant. By highlighting both historical lessons and current real-world examples, I hope to inspire readers to reflect on their own lives and the impact they can have by embodying these values. After all, even the smallest acts of kindness or integrity can make a significant difference in today’s world.
